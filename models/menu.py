9401be41aebd8c76c25cd22f97c064c46f13703b
Y1469492 <Tyler@tylers-mbp.home>
2015-11-21 16:53:14 +0000

    Set app name as Longboxes

--- models/menu.py
+++ models/menu.py
@@ -5,17 +5,15 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('Longboxes 📦'),
+response.title = "Longboxes"
+response.logo = A(B(response.title + ' 📦'),
                   _class="navbar-brand",_href=URL('default', 'index'),
                   _id="web2py-logo")
-response.title = request.application.replace('_',' ').title()
-response.subtitle = ''
+
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
-response.meta.keywords = 'web2py, python, framework'
-response.meta.generator = 'Web2py Web Framework'
+response.meta.author = 'Y1469492'
+response.meta.description = "A comic collector's favourite tool."
 
 ## your http://google.com/analytics id
 #response.google_analytics_id = None
