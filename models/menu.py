475f021bcabce5552d4a2f949ffff275b2a68ab0
Y1469492 <Y1469492@exams.cs>
2015-12-01 12:17:10 +0000

    Change menu bar

--- models/menu.py
+++ models/menu.py
@@ -24,15 +24,13 @@ response.meta.description = "A comic collector's favourite tool."
 
 if auth.is_logged_in():
     response.menu = [
-        (T('Collection'), False, URL('collection', 'index'), []),
-        (T('Comics'), False, None, [
-            (T('Search'), False, URL('collection', 'search')),
-            (T('View All'), False, URL('collection', 'all'))
-            ]),
+        (T('Your Collection'), False, URL('collection', 'index'), []),
+        (T('Your Comics'), False, URL('collection', 'all'), []),
+        (T('Search'), False, URL('collection', 'search'), []),
         (T('New'), False, None, [
             (T('Box'), False, URL('box', 'new')),
             (T('Comic'), False, URL('comic', 'new'))
-            ]),
+            ])
     ]
 
 if "auth" in locals(): auth.wikimenu()
